7ecc730a2babd7042e43d14e0057a5d6d27e4315
arturboyun <arturboyun@gmail.com>
2021-11-08 20:08:31 +0200

    dev and production baseUrl

--- frontend/env.js
+++ frontend/env.js
@@ -1,13 +1,10 @@
-// const env = process.env.VUE_APP_ENV;
+const env = process.env.NUXT_ENV_IS;
 
-// let envApiUrl = "";
-// if (env === 'production') {
-//   envApiUrl = "https://chili-lingerie.com";
-// } else {
-//   envApiUrl = "http://localhost";
-// }
+let envApiUrl = "";
+if (env === 'production') {
+  envApiUrl = "https://chili-lingerie.com";
+} else {
+  envApiUrl = "http://localhost";
+}
 
-// console.log(env);
-// console.log(envApiUrl);
-
-export const apiUrl = "https://chili-lingerie.com";
+export const apiUrl = envApiUrl;
